4402ede88e473ee1db6abe5365f4ac9247b490e2
Michael Duarte <michael.duarte@hotmail.com>
2018-02-03 11:31:54 -0800

    Make ScalaShop faster by making collections .par

--- kmeans/src/main/scala/kmeans/fun/IndexedColors.scala
+++ kmeans/src/main/scala/kmeans/fun/IndexedColors.scala
@@ -22,8 +22,8 @@ class IndexedColorFilter(initialImage: Img,
   private var steps = 0
 
   // What could we do here to speed up the computation?
-  val points = imageToPoints(initialImage)
-  val means = initializeIndex(colorCount, points)
+  val points = imageToPoints(initialImage).par
+  val means = initializeIndex(colorCount, points).par
 
   /* The work is done here: */
   private val newMeans = kMeans(points, means, 0.01)
@@ -49,7 +49,6 @@ class IndexedColorFilter(initialImage: Img,
       pts += point
       dst(x, y) = rgba(point.x, point.y, point.z, 1d)
     }
-
     dst
   }
 
