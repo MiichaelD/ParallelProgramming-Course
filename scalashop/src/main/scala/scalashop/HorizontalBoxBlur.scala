7fa389fe5618d59420f21b516a2154b25f5dc9c4
Michael Duarte <michael.duarte@hotmail.com>
2018-04-05 21:35:11 -0700

    Update box blur to process at least 1 col/row

--- scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
+++ scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -63,7 +63,7 @@ object HorizontalBoxBlur {
    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
     // TODO implement using the `task` construct and the `blur` method
-    val rowsPerTask = src.height / numTasks
+    val rowsPerTask = Math.max(1, src.height / numTasks)
     val splittingPoints = Range(0, src.height) by rowsPerTask
     val tasks = splittingPoints.map(sp => {
       task {
--- scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
+++ scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -64,7 +64,7 @@ object VerticalBoxBlur {
    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
     // TODO implement using the `task` construct and the `blur` method
-    val colsPerTask = src.width / numTasks
+    val colsPerTask = Math.max(1, src.width / numTasks)
     val splittingPoints = Range(0, src.width) by colsPerTask
     val tasks = splittingPoints.map(sp => {
       task {
